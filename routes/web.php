c698b7e9e181cab56cf5952fb62d83bda2b51adc
masaharu.hukuyama <hukuyama-msaharu@gmail.com>
2023-05-29 07:13:08 +0000

    CRUD messages

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\MessagesController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +14,22 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [MessagesController::class, 'index']);
+Route::resource('messages', MessagesController::class);
+
+// CRUD
+// メッセージの個別詳細ページ表示
+Route::get('messages/{id}', [MessagesController::class, 'show'])->name('messages.show');
+// メッセージの新規登録を処理（新規登録画面を表示するためのものではありません）
+Route::post('messages', [MessagesController::class, 'store'])->name('messages.store');
+// メッセージの更新処理（編集画面を表示するためのものではありません）
+Route::put('messages/{id}', [MessagesController::class, 'update'])->name('messages.update');
+// メッセージを削除
+Route::delete('messages/{id}', [MessagesController::class, 'destroy'])->name('messages.destory');
+
+// index: showの補助ページ
+Route::get('messages', [MessagesController::class, 'index'])->name('messages.index');
+// create: 新規作成用のフォームページ
+Route::get('messages/create', [MessagesController::class, 'create'])->name('messages.create');
+// edit: 更新用のフォームページ
+Route::get('messages/{id}/edit', [MessagesController::class, 'edit'])->name('messages.edit');
